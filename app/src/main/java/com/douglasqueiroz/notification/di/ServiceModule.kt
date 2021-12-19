8fa8ba43158be70d279a5ff73bbe196ba8e45d74
Douglas Queiroz <douglasq16@gmail.com>
2021-12-19 12:02:59 +0000

    Create ServiceModule

--- app/src/main/java/com/douglasqueiroz/notification/di/ServiceModule.kt
+++ app/src/main/java/com/douglasqueiroz/notification/di/ServiceModule.kt
@@ -2,7 +2,9 @@ package com.douglasqueiroz.notification.di
 
 import org.koin.dsl.module
 
-object ViewModelModule {
+object ServiceModule {
 
-    fun get() = module {}
+    fun get() = module {
+
+    }
 }
