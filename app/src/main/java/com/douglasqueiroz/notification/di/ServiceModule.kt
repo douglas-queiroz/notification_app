434bd49a4155708c732e3049a86d930dc8d8dd79
Douglas Queiroz <douglasq16@gmail.com>
2021-12-19 12:41:09 +0000

    Add NotificationListernerConnection into ServiceModule

--- app/src/main/java/com/douglasqueiroz/notification/di/ServiceModule.kt
+++ app/src/main/java/com/douglasqueiroz/notification/di/ServiceModule.kt
@@ -1,10 +1,12 @@
 package com.douglasqueiroz.notification.di
 
+import com.douglasqueiroz.notification.service.NotificationListenerConnection
+import com.douglasqueiroz.notification.service.NotificationListenerConnectionImpl
 import org.koin.dsl.module
 
 object ServiceModule {
 
     fun get() = module {
-
+        single<NotificationListenerConnection> { NotificationListenerConnectionImpl() }
     }
 }
