5f24266a7884da7135aab1524f629bf49edc8f53
Douglas Queiroz <douglasq16@gmail.com>
2021-12-19 11:26:11 +0000

    Create a constructor on NotificationDto to map StatusBarNotification

--- app/src/main/java/com/douglasqueiroz/notification/dto/NotificationDto.kt
+++ app/src/main/java/com/douglasqueiroz/notification/dto/NotificationDto.kt
@@ -1,7 +1,18 @@
 package com.douglasqueiroz.notification.dto
 
+import android.app.Notification
+import android.graphics.drawable.Icon
+import android.service.notification.StatusBarNotification
+
 data class NotificationDto(
     val title: String,
     val content: String,
-    val icon: String
-)
+    val icon: Icon
+) {
+
+    constructor(statusBarNotification: StatusBarNotification): this(
+        title = statusBarNotification.notification.extras.getString(Notification.EXTRA_TITLE) ?: "No Title",
+        content = statusBarNotification.notification.extras.getString(Notification.EXTRA_TEXT) ?: "No Content",
+        icon = statusBarNotification.notification.smallIcon,
+    )
+}
