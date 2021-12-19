45f86ef068e794a6f0c9490da5e16ad116e96814
Douglas Queiroz <douglasq16@gmail.com>
2021-12-19 12:41:47 +0000

    Bind NotificationListener from MainActivity

--- app/src/main/java/com/douglasqueiroz/notification/ui/MainActivity.kt
+++ app/src/main/java/com/douglasqueiroz/notification/ui/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.douglasqueiroz.notification.ui
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.douglasqueiroz.notification.R
+import com.douglasqueiroz.notification.service.NotificationListener
+import com.douglasqueiroz.notification.service.NotificationListenerConnection
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private val connection: NotificationListenerConnection by inject()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Intent(this, NotificationListener::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
     }
 }
